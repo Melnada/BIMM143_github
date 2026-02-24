014cd928e893c8989939062b4c23df117ed29437
melanda aboueid <mel@melandas-Laptop.local>
2026-02-24 12:24:50 -0800

    + class 4,7,8

---
+++ Class04/class04.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "Class 04 Into to R"
+#' author: "Melanda Aboueid"
+#' date: "Jan 15th, 2026"
+#' ---
+
+
+x <- 1:50
+plot(x, sin(x))
+
+plot(x, sin(x), typ="l", col="purple", lwd=2)
+
+log(10, base = 10)
